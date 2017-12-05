d537f6b84eb4aa4bf617acb77ab7b38efb37e778
Greg Connell <greg.connell@ff0000.com>
2017-12-04 18:46:03 -0800

    updating test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,12 @@
 const unixPathLiteral = require('../index.js');
 
 const command = `Project/ESPN Wimbledon 2017 -- CLONE/creative (version 1 & '2')/my-bash-script.sh`;
+
+// literal
+const literal = unixPathLiteral(command);
+console.log(literal);
+
 const output = {
-	str: unixPathLiteral(command)
+	literal: literal
 }
 console.log(output);
